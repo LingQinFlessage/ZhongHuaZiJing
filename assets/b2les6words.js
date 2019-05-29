8930f914c9a5c0f7caf5df602ab3de24cd512811
LingQinFlessage <bill.qinjin@gmail.com>
2019-05-29 18:40:53 +0200

    b2 les6 js

---
+++ assets/b2les6words.js
@@ -0,0 +1,56 @@
+var words={
+	les6:{
+		title:"体育",
+		sentences:[
+[{char:"奥",level:3},
+{char:"运",level:1},
+{char:"竞",level:2},
+{char:"技",level:1}],
+[{char:"淘",level:4},
+{char:"汰",level:5},
+{char:"筛",level:4},
+{char:"选",level:2}],
+[{char:"跨",level:2},
+{char:"跃",level:2},
+{char:"短",level:1},
+{char:"跑",level:1}],
+[{char:"蹦",level:4},
+{char:"跳",level:1},
+{char:"撑",level:3},
+{char:"竿",level:4}],
+[{char:"铁",level:2},
+{char:"饼",level:2},
+{char:"标",level:2},
+{char:"枪",level:2}],
+[{char:"垒",level:4},
+{char:"足",level:1},
+{char:"排",level:1},
+{char:"篮",level:1}],
+[{char:"汽",level:1},
+{char:"车",level:1},
+{char:"拉",level:1},
+{char:"力",level:1}],
+[{char:"驾",level:1},
+{char:"舢",level:6},
+{char:"驶",level:3},
+{char:"帆",level:4}],
+[{char:"刀",level:1},
+{char:"锤",level:4},
+{char:"棍",level:3},
+{char:"棒",level:3}],
+[{char:"钩",level:3},
+{char:"爪",level:4},
+{char:"杖",level:5},
+{char:"鞭",level:4}],,
+[{char:"动",level:1}
+{char:"静",level:1},
+{char:"迂",level:5},
+{char:"回",level:1}],,
+[{char:"摔",level:2}
+{char:"跤",level:6},
+{char:"击",level:2},
+{char:"拳",level:3}]
+		]
+
+	}
+}
