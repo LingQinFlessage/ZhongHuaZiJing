81fd97d1b0464d84fd6ac68cbdf1de1bae6f30d8
LingQinFlessage <bill.qinjin@gmail.com>
2019-06-05 19:33:04 +0200

    Add files via upload

---
+++ assets/b2les15words.js
@@ -0,0 +1,28 @@
+var words={
+	les14:{
+		title:"提手旁",
+		sentences:[
+[{char:"技 ",level:1},
+{char:"拉 ",level:1},
+{char:"摔  ",level:1},
+{char:"探",level:2}],
+[{char:"拒",level:2},
+{char:"招",level:2},
+{char:"把",level:1},
+{char:"摇",level:2}],
+[{char:"挥",level:2},
+{char:"捡",level:2},
+{char:"摘",level:2},
+{char:"打",level:1}],
+[{char:"接",level:1},
+{char:"插",level:2},
+{char:"扮",level:2},
+{char:"投",level:2}],
+[{char:"担",level:2},
+{char:"抬",level:1},
+{char:"扛",level:2},
+{char:"搬",level:1}]
+		]
+
+	}
+}
