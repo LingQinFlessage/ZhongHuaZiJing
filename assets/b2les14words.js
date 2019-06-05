12dfacf39b1a2913ce4864f699694aea5a1c8420
LingQinFlessage <bill.qinjin@gmail.com>
2019-06-05 19:12:34 +0200

    Add files via upload

---
+++ assets/b2les14words.js
@@ -0,0 +1,20 @@
+var words={
+	les14:{
+		title:"易混字",
+		sentences:[
+[{char:"偏",level:2},
+{char:"篇",level:1},
+{char:"遍",level:1},
+{char:"编",level:2}],
+[{char:"翻",level:1},
+{char:"播",level:2},
+{char:"番",level:3},
+{char:"乔",level:4}],
+[{char:"娇",level:4},
+{char:"矫",level:5},
+{char:"稼",level:2},
+{char:"嫁",level:4}]
+		]
+
+	}
+}
